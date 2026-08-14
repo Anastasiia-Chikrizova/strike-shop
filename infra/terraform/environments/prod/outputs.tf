c978042c09ca921131b83fdd524fe44c5e69d1e7
Anastasiia Chikrizova <anastasiia.chikrizova@gmail.com>
2026-08-14 17:15:36 +0300

    Add GitHub Actions OIDC setup for AWS deploys and refactor workflows

--- infra/terraform/environments/prod/outputs.tf
+++ infra/terraform/environments/prod/outputs.tf
@@ -12,3 +12,8 @@ output "ssm_command" {
   description = "Ready-to-paste command to open a shell on the instance."
   value       = "aws ssm start-session --target ${module.app_instance.instance_id} --region ${var.region}"
 }
+
+output "github_actions_role_arn" {
+  description = "ARN of the role that GitHub Actions assumes via OIDC for deployment."
+  value       = aws_iam_role.github_actions_deploy.arn
+}
