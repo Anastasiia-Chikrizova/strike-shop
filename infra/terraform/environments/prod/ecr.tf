779f6d2de9ea7ba7c539e3b1c72437e429e0ce8c
Anastasiia Chikrizova <anastasiia.chikrizova@gmail.com>
2026-08-14 18:04:57 +0300

    Migrate Docker image registry from GHCR to AWS ECR, configure ECR repositories with lifecycle policies, and update IAM roles and workflows accordingly.

---
+++ infra/terraform/environments/prod/ecr.tf
@@ -0,0 +1,75 @@
+locals {
+  ecr_repos = ["backend", "storefront"]
+}
+
+resource "aws_ecr_repository" "this" {
+  for_each = toset(local.ecr_repos)
+  name     = "${var.project}/${each.value}"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  for_each   = aws_ecr_repository.this
+  repository = each.value.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expire untagged images after 1 day"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 1
+        }
+        action = { type = "expire" }
+      },
+      {
+        rulePriority = 2
+        description  = "Keep only the last 10 tagged images"
+        selection = {
+          tagStatus      = "tagged"
+          tagPatternList = ["*"]
+          countType      = "imageCountMoreThan"
+          countNumber    = 10
+        }
+        action = { type = "expire" }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions_ecr_push" {
+  name = "ecr-push"
+  role = aws_iam_role.github_actions_deploy.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "GetAuthToken"
+        Effect   = "Allow"
+        Action   = "ecr:GetAuthorizationToken"
+        Resource = "*"
+      },
+      {
+        Sid    = "PushPull"
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+        ]
+        Resource = [for repo in aws_ecr_repository.this : repo.arn]
+      }
+    ]
+  })
+}
